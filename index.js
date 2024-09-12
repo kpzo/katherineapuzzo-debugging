35e0c5d9d409e9f9b5255ea4a5acf99d67804f0c
kpzo <153002207+kpzo@users.noreply.github.com>
2024-09-11 22:36:56 -0400

    fixed it!
    i think i got all the bugs.. it works properly now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const guessInput = document.getElementById('guess');
 const submitButton = document.getElementById('submit');
 const resetButton = document.getElementById('reset');
-const messages = document.getElementsByClassName('message');
+const messages = document.querySelectorAll('p'); // wasn't selecting the proper elements from html
 const tooHighMessage = document.getElementById('too-high');
 const tooLowMessage = document.getElementById('too-low');
 const maxGuessesMessage = document.getElementById('max-guesses');
@@ -12,6 +12,9 @@ let targetNumber;
 let attempts = 0;
 const maxNumberOfAttempts = 5;
 
+// was missing the definition of the array that was used in the hideAllMessages function
+const messageArray = Array.from(messages);
+
 // Returns a random number from min (inclusive) to max (exclusive)
 // Usage:
 // > getRandomNumber(1, 50)
@@ -20,14 +23,36 @@ const maxNumberOfAttempts = 5;
 // <- 11
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// was '<=' but should have just been '<' below, also moved the function to the top of the file
+function hideAllMessages() {
+  for (let elementIndex = 0; elementIndex < messageArray.length; elementIndex++) {
+    messageArray[elementIndex].style.display = 'none';
+  }
 }
+
+function setup() { // moved this function to the top of the file
+  // Get random number
+  targetNumber = getRandomNumber(1, 100);
+  console.log(`target number: ${targetNumber}`);
+
+  // Reset number of attempts - this was using the wrong variable name
+  attempts = 0;
+
+  // Enable the input and submit button
+  submitButton.disabled = false;
+  guessInput.disabled = false;
+
+  hideAllMessages();
+  reset.style.display = 'none';
+}
+
 
 function checkGuess() {
   // Get value from guess input element
-  const guess = parseInt(guessInput.value, 10);
+  const guess = parseInt(guessInput.value);
   attempts = attempts + 1;
-
-  hideAllMessages();
 
   if (guess === targetNumber) {
     numberOfGuessesMessage.style.display = '';
@@ -40,20 +65,27 @@ function checkGuess() {
   }
 
   if (guess !== targetNumber) {
+
+    hideAllMessages(); // this was in the wrong section of the function
+
     if (guess < targetNumber) {
       tooLowMessage.style.display = '';
     } else {
-      tooLowMessage.style.display = '';
+      tooHighMessage.style.display = ''; // this repeated the tooLowMessage variable, and needed to be changed to output the proper message
     }
 
     const remainingAttempts = maxNumberOfAttempts - attempts;
 
     numberOfGuessesMessage.style.display = '';
+    if (remainingAttempts < 0) {
+      remainingAttempts = 0;
+    }
+
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
 
-  if (attempts ==== maxNumberOfAttempts) {
-    submitButton.disabled = true;
+  if (attempts === maxNumberOfAttempts) {
+    submitButton.disabled = true; // there was a typo here
     guessInput.disabled = true;
   }
 
@@ -62,27 +94,6 @@ function checkGuess() {
   resetButton.style.display = '';
 }
 
-function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
-    messages[elementIndex].style.display = 'none';
-  }
-}
-
-funtion setup() {
-  // Get random number
-  targetNumber = getRandomNumber(1, 100);
-  console.log(`target number: ${targetNumber}`);
-
-  // Reset number of attempts
-  maxNumberOfAttempts = 0;
-
-  // Enable the input and submit button
-  submitButton.disabeld = false;
-  guessInput.disabled = false;
-
-  hideAllMessages();
-  resetButton.style.display = 'none';
-}
 
 submitButton.addEventListener('click', checkGuess);
 resetButton.addEventListener('click', setup);
